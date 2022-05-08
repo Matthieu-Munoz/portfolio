b7efc20abbc6901832086d3e425787489af528a4
Matthieu-Munoz <matthieu.munoz.perso@gmail.com>
2022-05-08 16:23:17 +0200

    init

---
+++ src/components/App/index.js
@@ -0,0 +1,17 @@
+// Dependencies
+
+// React-Redux
+
+// Styles
+import './app.scss';
+
+
+function App() {
+  return (
+    <div className="app">
+      Mon portfolio
+    </div>
+  );
+}
+
+export default App;
---
+++ src/store/index.js
@@ -0,0 +1,13 @@
+import { legacy_createStore as createStore, applyMiddleware, compose } from 'redux';
+import reducer from '@/reducers';
+import globalMiddleWare from '@/middlewares/global';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const enhancers = composeEnhancers(
+  applyMiddleware(globalMiddleWare),
+);
+
+const store = createStore(reducer, enhancers);
+
+export default store;
