4df21f0d1092bda5c4d4f1b7f90b9fbd556ba16b
Matthieu Munoz <72809504+Matthieu-Munoz@users.noreply.github.com>
2022-06-02 00:29:08 +0200

    adding project and infos

--- src/components/Modal/ProjectInfo/index.js
+++ src/components/Modal/ProjectInfo/index.js
@@ -11,10 +11,9 @@ import "./projectinfo.scss"
 
 function ProjectInfo() {
     const { project } = useSelector((state) => state.projects);
-    console.log(project);
     return (
         <div className="projectinfo">
-            <img src={project.logo} alt={`logo of ${project.title}`} className="projectinfo__logo" />
+            <a href={project.url} target="_blank" rel="noreferrer" ><img src={project.logo} alt={`logo of ${project.title}`} className="projectinfo__logo" /></a>
             <div className="projectinfo__sep" />
             <div className="projectinfo__ctn">
                 <div className="projectinfo__ctn__info projectinfo__ctn__info--desc">
@@ -24,23 +23,23 @@ function ProjectInfo() {
                 <div className="projectinfo__ctn__info projectinfo__ctn__info--proj">
                     <h3 className="projectinfo__ctn__info__title">Le projet :</h3>
                     <div className="projectinfo__ctn__info__text">
-                        <p className="projectinfo__ctn__info__text__para">
+                        {project.github && <div className="projectinfo__ctn__info__text__para">
                             <h4 className="projectinfo__ctn__info__text__subtitle">Code :</h4><a className="projectinfo__ctn__info__text__link" href={project.github} target="_blank" rel="noreferrer">Lien GitHub</a>
-                        </p>
-                        <p className="projectinfo__ctn__info__text__para">
+                        </div>}
+                        <div className="projectinfo__ctn__info__text__para">
                             <h4 className="projectinfo__ctn__info__text__subtitle">Date de réalisation :</h4><span dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(project.date) }} ></span>
-                        </p>
-                        <p className="projectinfo__ctn__info__text__para">
+                        </div>
+                        <div className="projectinfo__ctn__info__text__para">
                             <h4 className="projectinfo__ctn__info__text__subtitle">Contexte :</h4><span dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(project.context) }} ></span>
-                        </p>
-                        <p className="projectinfo__ctn__info__text__para">
+                        </div>
+                        <div className="projectinfo__ctn__info__text__para">
                             <h4 className="projectinfo__ctn__info__text__subtitle">Compétences utilisées :</h4>
                             <div className="projectinfo__ctn__info__text__skills">
                                 {project.skills && project.skills.map((skill) => {
-                                    return <span className="projectinfo__ctn__info__text__skills__skill">{skill}</span>;
+                                    return <span className="projectinfo__ctn__info__text__skills__skill" key={skill}>{skill}</span>;
                                 })}
                             </div>
-                        </p>
+                        </div>
                     </div>
                 </div>
             </div>
