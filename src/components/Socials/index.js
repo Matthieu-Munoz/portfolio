23e2e1f8fdc1a47ea944f1fbc022cb333b2b7825
Matthieu-Munoz <matthieu.munoz.perso@gmail.com>
2022-06-10 17:46:12 +0200

    cleanup js

--- src/components/Socials/index.js
+++ src/components/Socials/index.js
@@ -1,41 +1,74 @@
 // Dependencies
-import { useDispatch } from 'react-redux';
-import { AiFillGithub, AiFillTwitterCircle, AiFillMail, AiFillLinkedin } from "react-icons/ai";
-import * as Scroll from 'react-scroll';
+import { useDispatch } from "react-redux";
+import {
+  AiFillGithub,
+  AiFillTwitterCircle,
+  AiFillMail,
+  AiFillLinkedin,
+} from "react-icons/ai";
+import * as Scroll from "react-scroll";
 // Local | React-Redux
-import { toggleSection } from '@/actions/app';
+import { toggleSection } from "@/actions/app";
 // styles
-import "./socials.scss"
+import "./socials.scss";
 
 function Socials() {
-    const dispatch = useDispatch();
-    const scroller = Scroll.scroller;
-    const handleClickContact = (evt) => {
-        evt.preventDefault();
-        dispatch(toggleSection('contact', true));
-        scroller.scrollTo('contact', {
-            duration: 500,
-            smooth: true,
-            ignoreCancelEvents: true,
-        })
-    }
-    return (
-        <div className="socials">
-            <a href="/" className="socials__link" title="lien vers la section contact" onClick={handleClickContact} aria-label="Contact" data-cooltipz-dir="bottom">
-                <AiFillMail className="socials__link--icon" />
-            </a>
-            <a href="https://github.com/Matthieu-Munoz" title="lien vers mon Github" target="blank" className="socials__link" aria-label="Github" data-cooltipz-dir="bottom">
-                <AiFillGithub className="socials__link--icon" />
-            </a>
-            <a href="https://twitter.com/Matthieu_Mnz" title="lien vers mon Twitter" target="blank" className="socials__link" aria-label="Twitter" data-cooltipz-dir="bottom">
-                <AiFillTwitterCircle className="socials__link--icon" />
-            </a>
-            <a href="https://www.linkedin.com/in/matthieu-munoz-798b33184/" title="lien vers mon Linkedin" target="blank" className="socials__link" aria-label="Linkedin" data-cooltipz-dir="bottom">
-                <AiFillLinkedin className="socials__link--icon" />
-            </a>
-            <span className="socials__dash" />
-        </div>
-    );
+  const dispatch = useDispatch();
+  const scroller = Scroll.scroller;
+  const handleClickContact = (evt) => {
+    evt.preventDefault();
+    dispatch(toggleSection("contact", true));
+    scroller.scrollTo("contact", {
+      duration: 500,
+      smooth: true,
+      ignoreCancelEvents: true,
+    });
+  };
+  return (
+    <div className="socials">
+      <a
+        href="/"
+        className="socials__link"
+        title="lien vers la section contact"
+        onClick={handleClickContact}
+        aria-label="Contact"
+        data-cooltipz-dir="bottom"
+      >
+        <AiFillMail className="socials__link--icon" />
+      </a>
+      <a
+        href="https://github.com/Matthieu-Munoz"
+        title="lien vers mon Github"
+        target="blank"
+        className="socials__link"
+        aria-label="Github"
+        data-cooltipz-dir="bottom"
+      >
+        <AiFillGithub className="socials__link--icon" />
+      </a>
+      <a
+        href="https://twitter.com/Matthieu_Mnz"
+        title="lien vers mon Twitter"
+        target="blank"
+        className="socials__link"
+        aria-label="Twitter"
+        data-cooltipz-dir="bottom"
+      >
+        <AiFillTwitterCircle className="socials__link--icon" />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/matthieu-munoz-798b33184/"
+        title="lien vers mon Linkedin"
+        target="blank"
+        className="socials__link"
+        aria-label="Linkedin"
+        data-cooltipz-dir="bottom"
+      >
+        <AiFillLinkedin className="socials__link--icon" />
+      </a>
+      <span className="socials__dash" />
+    </div>
+  );
 }
 
 export default Socials;
