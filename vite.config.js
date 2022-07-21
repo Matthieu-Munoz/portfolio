ddaa30e5fb92f55b8e3777af142d7b74f1eb7b45
Matthieu-Munoz <matthieu.munoz.pro@gmail.com>
2022-07-21 09:10:34 +0200

    animation in file

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import svgr from "@honkhonk/vite-plugin-svgr";
 import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
 
@@ -18,7 +17,6 @@ export default defineConfig({
   },
   plugins: [
     react(),
-    svgr(),
     VitePWA({
       registerType: "autoUpdate",
       devOptions: {
@@ -56,19 +54,5 @@ export default defineConfig({
   ],
   build: {
     chunkSizeWarningLimit: 1000,
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return id
-              .toString()
-              .split("node_modules/")[1]
-              .split("/")[0]
-              .toString();
-          }
-        },
-      },
-    },
   },
 });
