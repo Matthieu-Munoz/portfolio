9b55d5bf6139de1b0390fbc416fa9c21deb1bf1d
Matthieu-Munoz <matthieu.munoz.pro@gmail.com>
2023-01-03 14:20:39 +0100

    init

--- src/entry-server.jsx
+++
@@ -1,9 +0,0 @@
-// Dependencies
-import ReactDOMServer from "react-dom/server";
-// Local | React-Redux
-import App from "./app";
-// Styles
-
-export const render = () => {
-  return ReactDOMServer.renderToString(<App />);
-};
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,11 @@
-// Dependencies
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import store from "./store";
-// Local | React-Redux
 import App from "./app";
-// Styles
 import "./styles/index.scss";
 
-ReactDOM.hydrateRoot(
-  document.getElementById("root"),
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
