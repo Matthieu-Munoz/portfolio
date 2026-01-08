537bc581a22a2bf207a77e4f9c695da46aea5708
MatthieuMnz <m.munoz@icarsoft-france.fr>
2026-01-08 14:43:54 +0100

    Refactor project configuration and dependencies; remove ESLint config, update package versions, and enhance Field component with new textarea handling.

--- components/Field/index.jsx
+++ components/Field/index.jsx
@@ -1,10 +1,10 @@
 import classNames from "classnames";
 import { AiOutlineExclamationCircle } from "react-icons/ai";
 import { PropTypes } from "prop-types";
-import TextareaAutosize from "react-autosize-textarea/lib";
+import TextareaAutosize from "react-textarea-autosize";
 // styles
 
-export function Field({ label, name, type, onChange, tip, rhfregister, rhferror }) {
+export function Field({ label, name, type = "text", onChange, tip = "", rhfregister, rhferror }) {
   const cssClass = classNames("input-container", { "input-error": rhferror[name] });
 
   const handleChange = (evt) => {
@@ -18,10 +18,10 @@ export function Field({ label, name, type, onChange, tip, rhfregister, rhferror 
             className="input-container__textarea"
             name={name}
             required
-            style={{ maxHeight: 100, resize: "none" }}
+            maxRows={4}
+            style={{ resize: "none" }}
             {...rhfregister(name, { required: true })}
             id={name}
-            error={rhferror[name]}
           />
           <label htmlFor={name}>{label}</label>
           <AiOutlineExclamationCircle className="error__icon" aria-label={tip} data-cooltipz-dir="top" />
@@ -29,7 +29,7 @@ export function Field({ label, name, type, onChange, tip, rhfregister, rhferror 
         </div>
       ) : (
         <div className={cssClass}>
-          <input type={type} {...rhfregister(name, { required: true })} id={name} error={rhferror[name]} />
+          <input type={type} {...rhfregister(name, { required: true })} id={name} />
           <label htmlFor={name}>{label}</label>
           <div className="error__icon" aria-label={tip} data-cooltipz-dir="left">
             <AiOutlineExclamationCircle className="error__icon" />
@@ -46,9 +46,4 @@ Field.propTypes = {
   name: PropTypes.string.isRequired,
   type: PropTypes.string,
   tip: PropTypes.string,
-};
-
-Field.defaultProps = {
-  type: "text",
-  tip: "",
 };
